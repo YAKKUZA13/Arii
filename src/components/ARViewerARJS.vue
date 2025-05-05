d4c9d1e3593bfc36d58e34edb30f009f11de5d65
yakuza <steadler1703@yandex.ru>
2025-05-05 21:00:00 +0300

    fix: отрисовка груши

--- src/components/ARViewerARJS.vue
+++ src/components/ARViewerARJS.vue
@@ -4,18 +4,24 @@
       embedded
       arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;"
       renderer="logarithmicDepthBuffer: true;"
+      @arjs-marker-found="onMarkerFound"
     >
       <!-- Маркер для AR.js -->
       <a-marker preset="hiro">
         <!-- 3D модель -->
         <a-entity
-          gltf-model="https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-          scale="0.05 0.05 0.05"
+          gltf-model="/models/grusha.glb"
+          scale="0.5 0.5 0.5"
           position="0 0.5 0"
-          animation="property: rotation; to: 0 360 0; loop: true; dur: 10000;"
+          rotation="90 90 90"
+          animation="property: rotation; to: 0 90 0; loop: false; dur: 10000;"
         ></a-entity>
       </a-marker>
-      
+      <a-entity v-if="fixedModelPosition" 
+                :position="fixedModelPosition" 
+                gltf-model="/models/grusha.glb"
+                scale="0.5 0.5 0.5"
+                rotation="0 90 0"></a-entity>
       <!-- Камера -->
       <a-entity camera></a-entity>
     </a-scene>
@@ -31,12 +37,36 @@
 import { ref, onMounted } from 'vue'
 
 const isSupported = ref(true)
+const marker = ref(null)
+const model = ref(null)
+const fixedModelPosition = ref(null)
 
+const onMarkerFound = () => {
+  if (!fixedModelPosition.value) {
+    // Получаем мировые координаты маркера
+    const markerPosition = marker.value.object3D.position
+    const worldPosition = marker.value.object3D.parent.worldToLocal(markerPosition.clone())
+    
+    // Фиксируем позицию
+    fixedModelPosition.value = `${worldPosition.x} ${worldPosition.y} ${worldPosition.z}`
+    
+    // Скрываем оригинальную модель
+    model.value.object3D.visible = false
+  }
+}
 onMounted(() => {
   // Проверяем поддержку WebGL
   const canvas = document.createElement('canvas')
   const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
   isSupported.value = !!gl
+  AFRAME.registerComponent('persistent-position', {
+    tick: function() {
+      if (this.el.isPersisted) {
+        const camera = document.querySelector('[camera]').object3D
+        this.el.object3D.position.sub(camera.position)
+      }
+    }
+  })
 })
 </script>
 
